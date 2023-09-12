a3a547faf3b84c7bc19880414abe6f5e0199bc46
MPWSnow <144676859+MPWSnow@users.noreply.github.com>
2023-09-12 12:05:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output normalised data to a table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
